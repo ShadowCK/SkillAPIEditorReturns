eddf00faf0183ac3068824cdb3a6d5f930581eae
ShadowCK <540712348@qq.com>
2023-11-14 23:08:51 -0500

    Fixed filter.js to conform with ESLint rules

--- src/filter.js
+++ src/filter.js
@@ -2,80 +2,87 @@
  * Filters an input box for integers by setting this
  * as the input event listener.
  */
-function filterInt() {
+const filterInt = () => {
   // Get the element
-  var element = document.activeElement;
+  const element = document.activeElement;
 
   // Remove non-numeric characters
   element.value = element.value.replace(/[^0-9-]/g, '');
 
-  // Remove unnecessary 0's
-  for (var i = 0; i < element.value.length - 1; i++) {
-    var c = element.value.charAt(i);
-    if (element.value.charAt(i) == '0') {
+  // Remove unnecessary (leading) 0's
+  for (let i = 0; i < element.value.length - 1; i++) {
+    const char = element.value.charAt(i);
+    if (element.value.charAt(i) === '0') {
       element.value = element.value.replace('0', '');
       i--;
-    } else if (c != '-') {
+    } else if (char !== '-') {
       break;
     }
   }
 
   // Remove extra negative signs
   if (element.value.lastIndexOf('-') > 0) {
-    var negative = element.value.charAt(0) != '-';
+    // Note: original code was wrong with "var negative = element.value.charAt(0) != '-';"
+    const negative = element.value.charAt(0) === '-';
     element.value = element.value.replace(/-/g, '');
     if (negative) {
-      element.value = '-' + element.value;
+      element.value = `-${element.value}`;
     }
   }
 
   // Prevent it from being empty
-  if (element.value.length == 0 || (element.value.length == 1 && element.value.charAt(0) == '-')) {
+  if (
+    element.value.length === 0 ||
+    (element.value.length === 1 && element.value.charAt(0) === '-')
+  ) {
     element.value += '0';
   }
-}
+};
 
 /**
  * Filters an input box for doubles by setting this
  * as the input event listener.
  */
-function filterDouble() {
+const filterDouble = () => {
   // Get the data
-  var element = document.activeElement;
-  var negative = false;
-  var index = -1;
-  while ((index = element.value.indexOf('-', index + 1)) >= 0) {
+  const element = document.activeElement;
+
+  // Note: `negative` here is using a different checking approach than `filterInt`
+  let negative = false;
+  let index = -1;
+  index = element.value.indexOf('-', index + 1);
+  // Revert sign if there is a negative sign
+  while (index >= 0) {
     negative = !negative;
+    index = element.value.indexOf('-', index + 1);
   }
 
   // Remove non-numeric characters besides periods
-  var filtered = element.value.replace(/[^0-9\.-]/g, '');
-  if (filtered != element.value) {
+  const filtered = element.value.replace(/[^0-9.-]/g, '');
+  if (filtered !== element.value) {
     element.value = filtered;
   }
 
   // Remove unnecessary 0's
-  for (var i = 0; i < element.value.length - 1; i++) {
-    var c = element.value.charAt(i);
-    if (element.value.charAt(i) == '0' && element.value.charAt(i + 1) != '.') {
+  for (let i = 0; i < element.value.length - 1; i++) {
+    const char = element.value.charAt(i);
+    if (element.value.charAt(i) === '0' && element.value.charAt(i + 1) !== '.') {
       element.value = element.value.replace('0', '');
       i--;
-    } else if (c != '-') {
+    } else if (char !== '-') {
       break;
     }
   }
 
-  // Remove extra negative signs
-  if (element.value.lastIndexOf('-') > 0) {
-    var negative = element.value.charAt(0) != '-';
-    element.value = element.value.replace(/-/g, '');
-    if (negative) {
-      element.value = '-' + element.value;
-    }
+  // Remove all negative signs
+  element.value = element.value.replace(/-/g, '');
+  // If the final number should be negative, add a negative sign at the start
+  if (negative) {
+    element.value = `-${element.value}`;
   }
 
   // Prevent it from being empty
-  if (element.value.length == 0 || (element.value.length == 1 && element.value == '-')) {
+  if (element.value.length === 0 || (element.value.length === 1 && element.value === '-')) {
     element.value += '0';
   }
-}
+};
