40741c37bf6083e04ecf1cfe419e87772db260eb
ShadowCK <540712348@qq.com>
2023-11-17 09:59:49 -0500

    Fixed the format in loader.js

--- src/loader.js
+++ src/loader.js
@@ -119,7 +119,7 @@ const depend = (script, callback) => {
   if (SCRIPT_TAGS[script]) {
     const data = SCRIPT_TAGS[script];
     if (data.loaded) {
-            if (callback) {
+      if (callback) {
         callback();
       }
     } else {
@@ -134,16 +134,18 @@ const depend = (script, callback) => {
   const foundScript = _.findWhere(scripts, { src });
   if (foundScript) {
     SCRIPT_TAGS[script] = { tag: foundScript, loaded: true };
-    if (callback) {callback();}
+    if (callback) {
+      callback();
+    }
     return;
   }
-    // Otherwise, load the script
+  // Otherwise, load the script
   const scriptTag = document.createElement('script');
   scriptTag.id = script;
   scriptTag.type = 'text/javascript';
   if (callback) {
-        scriptTag.addEventListener('load', callback);
-      }
+    scriptTag.addEventListener('load', callback);
+  }
   scriptTag.addEventListener('load', SCRIPT_TAGS.onload);
   SCRIPT_TAGS[script] = { tag: scriptTag, loaded: false };
   scriptTag.src = src;
