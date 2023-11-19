6071010041e1f2b9874a36deada75411e1361552
ShadowCK <540712348@qq.com>
2023-11-18 21:59:27 -0500

    Adapted webpack config to ES module syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
-const path = require('path');
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+// Note: Cannot use __dirname and __filename in Node.js's ES module, 
+// because it's meant to be compatible with both Node.js and browser.
+// The following code is a workaround for this issue, only works in Node.js.
+// It's fine since we won't use this config file in browser.
+const __filename = fileURLToPath(new URL(import.meta.url));
+const __dirname = dirname(__filename);
 
-module.exports = {
+export default {
   entry: './src/main.js',
   mode: 'development',
   watchOptions: {
     aggregateTimeout: 200,
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
 };
