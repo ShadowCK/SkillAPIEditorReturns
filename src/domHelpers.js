8e657ee598173fb9f336f70903481ab2becc6773
ShadowCK <540712348@qq.com>
2023-11-24 20:08:29 -0500

    Modernized setupOptionList() in domHelpers.js

--- src/domHelpers.js
+++ src/domHelpers.js
@@ -19,6 +19,36 @@ const setSelectedOption = (option) => {
 };
 
 /**
+ * Saves text data to a file locally
+ *
+ * Code slightly modified from this page:
+ * https://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
+ */
+const saveToFile = (file, data) => {
+  const textFileAsBlob = new Blob([data], { type: 'text/plain;charset=utf-8' });
+
+  const downloadLink = document.createElement('a');
+  downloadLink.download = file;
+  downloadLink.text = 'Download File';
+  if (window.webkitURL != null) {
+    // Chrome allows the link to be clicked
+    // without actually adding it to the DOM.
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    // Firefox requires the link to be added to the DOM
+    // before it can be clicked.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = (e) => {
+      document.body.removeChild(e.target);
+    };
+    downloadLink.style.display = 'none';
+    document.body.appendChild(downloadLink);
+  }
+
+  downloadLink.click();
+};
+
+/**
  * Retrieves the current form being displayed.
  * @param {Array<string>} source - Optional. An array of form IDs to check. Defaults to ['builder', 'skillForm', 'componentChooser', 'triggerChooser'].
  * @returns {string|null} - The ID of the current form being displayed, or null if no form is found.
@@ -67,57 +97,63 @@ const setupOptionList = (div, list, type) => {
   // Reset div
   // FIXME: This seems unnecessary. The div should be empty already.
   div.innerHTML = '';
-  let x;
+
   let output = '';
-  const keys = Object.keys(list).sort();
-  for (let i = 0; i < keys.length; i++) {
-    x = keys[i];
-    if (i % 4 === 0) {
-      output += '| ';
-    }
-    output += `[[${list[x].name}|_${type.substr(0, 1).toUpperCase()}${type.substr(1)} ${
-      list[x].name
-    }]] | `;
-    if ((i + 1) % 4 === 0) {
-      output += '\n';
-    }
 
-    const h5 = document.createElement('h5');
-    if (list[x].premium) {
-      h5.className = 'premium';
-    }
-    if (list[x].container) {
-      h5.classList.add('container');
-    }
-    h5.textContent = list[x].name;
-    h5.component = list[x];
-    h5.addEventListener('click', () => {
-      const activeComponent = getActiveComponent();
-      const activeSkill = getActiveSkill();
-      debug.logIfAllowed(debug.levels.VERBOSE, '----- Active Component -----');
-      debug.logIfAllowed(debug.levels.VERBOSE, activeComponent);
-      debug.logIfAllowed(debug.levels.VERBOSE, '-----   Active Skill   -----');
-      debug.logIfAllowed(debug.levels.VERBOSE, activeSkill);
-      debug.logIfAllowed(debug.levels.VERBOSE, '----------------------------');
-      // If the user is adding a trigger that already exists, go back to the builder
-      if (activeComponent === activeSkill && activeSkill.usingTrigger(h5.component.name)) {
-        debug.logIfAllowed(debug.levels.WARN, 'Trigger already exists, going back to builder');
-        showSkillPage('builder');
-      } else {
-        showSkillPage('skillForm');
-        const component = h5.component.constructor
-          ? new h5.component.constructor()
-          : h5.component.supplier();
-        component.parent = activeComponent;
-        activeComponent.components.push(component);
-        component.createBuilderHTML(activeComponent.html);
-        component.createFormHTML();
+  Object.entries(list)
+    .sort(([keyA], [keyB]) =>
+      // This sorts using only unicode values
+      // keyA > keyB ? 1 : keyA < keyB ? -1 : 0;
+      // * This respects locale. For example, it can sort Chinese characters based on their pinyin.
+      keyA.localeCompare(keyB),
+    )
+    .forEach(([, value], index) => {
+      if (index % 4 === 0) {
+        output += '| ';
       }
-    });
-    div.appendChild(h5);
-  }
+      output += `[[${value.name}|_${type.substr(0, 1).toUpperCase()}${type.substr(1)} ${
+        value.name
+      }]] | `;
+      if ((index + 1) % 4 === 0) {
+        output += '\n';
+      }
 
-  // saveToFile('wiki_' + type + '.txt', output);
+      const h5 = document.createElement('h5');
+      if (value.premium) {
+        h5.className = 'premium';
+      }
+      if (value.container) {
+        h5.classList.add('container');
+      }
+      h5.textContent = value.name;
+      h5.component = value;
+      h5.addEventListener('click', () => {
+        const activeComponent = getActiveComponent();
+        const activeSkill = getActiveSkill();
+        debug.logIfAllowed(debug.levels.VERBOSE, '----- Active Component -----');
+        debug.logIfAllowed(debug.levels.VERBOSE, activeComponent);
+        debug.logIfAllowed(debug.levels.VERBOSE, '-----   Active Skill   -----');
+        debug.logIfAllowed(debug.levels.VERBOSE, activeSkill);
+        debug.logIfAllowed(debug.levels.VERBOSE, '----------------------------');
+        // If the user is adding a trigger that already exists, go back to the builder
+        if (activeComponent === activeSkill && activeSkill.usingTrigger(h5.component.name)) {
+          debug.logIfAllowed(debug.levels.WARN, 'Trigger already exists, going back to builder');
+          showSkillPage('builder');
+        } else {
+          showSkillPage('skillForm');
+          const component = h5.component.constructor
+            ? new h5.component.constructor()
+            : h5.component.supplier();
+          component.parent = activeComponent;
+          activeComponent.components.push(component);
+          component.createBuilderHTML(activeComponent.html);
+          component.createFormHTML();
+        }
+      });
+      div.appendChild(h5);
+    });
+
+  // saveToFile(`wiki_${type}.txt`, output);
 };
 
 const refreshOptions = () => {
@@ -126,36 +162,6 @@ const refreshOptions = () => {
   setupOptionList(document.getElementById('targetOptions'), Target, Type.TARGET);
   setupOptionList(document.getElementById('conditionOptions'), Condition, Type.CONDITION);
   setupOptionList(document.getElementById('mechanicOptions'), Mechanic, Type.MECHANIC);
-};
-
-/**
- * Saves text data to a file locally
- *
- * Code slightly modified from this page:
- * https://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
- */
-const saveToFile = (file, data) => {
-  const textFileAsBlob = new Blob([data], { type: 'text/plain;charset=utf-8' });
-
-  const downloadLink = document.createElement('a');
-  downloadLink.download = file;
-  downloadLink.text = 'Download File';
-  if (window.webkitURL != null) {
-    // Chrome allows the link to be clicked
-    // without actually adding it to the DOM.
-    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-  } else {
-    // Firefox requires the link to be added to the DOM
-    // before it can be clicked.
-    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-    downloadLink.onclick = (e) => {
-      document.body.removeChild(e.target);
-    };
-    downloadLink.style.display = 'none';
-    document.body.appendChild(downloadLink);
-  }
-
-  downloadLink.click();
 };
 
 const switchToSkills = () => {
