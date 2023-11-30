4c959d30212e9a94365a46aaffd8c5daae88befb
ShadowCK <540712348@qq.com>
2023-11-30 00:59:08 -0500

    Added particle data for 1.8 and replaced outdated code that uses the same particle data for all versions (getParticles() were never used in the original code)

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,6 +1,6 @@
 import { ListValue, AttributeValue, DoubleValue, IntValue, StringValue } from '../input.js';
 
-import { getMaterials } from '../data/index.js';
+import { getMaterials, getParticles } from '../data/index.js';
 
 const appendOptional = (value) => {
   value.requireValue('use-effect', ['True']);
@@ -138,71 +138,13 @@ const addProjectileOptions = (component) => {
  */
 const addParticleOptions = (component) => {
   component.data.push(
-    new ListValue(
-      'Particle',
-      'particle',
-      [
-        'Angry Villager',
-        'Barrier',
-        'Block Crack',
-        'Bubble',
-        'Cloud',
-        'Crit',
-        'Damage Indicator',
-        'Death',
-        'Death Suspend',
-        'Dragon Breath',
-        'Drip Lava',
-        'Drip Water',
-        'Enchantment Table',
-        'End Rod',
-        'Ender Signal',
-        'Explode',
-        'Firework Spark',
-        'Flame',
-        'Footstep',
-        'Happy Villager',
-        'Heart',
-        'Huge Explosion',
-        'Hurt',
-        'Icon Crack',
-        'Instant Spell',
-        'Large Explode',
-        'Large Smoke',
-        'Lava',
-        'Magic Crit',
-        'Mob Spell',
-        'Mob Spell Ambient',
-        'Mobspawner Flames',
-        'Note',
-        'Portal',
-        'Potion Break',
-        'Red Dust',
-        'Sheep Eat',
-        'Slime',
-        'Smoke',
-        'Snowball Poof',
-        'Snow Shovel',
-        'Spell',
-        'Splash',
-        'Sweep Attack',
-        'Suspend',
-        'Town Aura',
-        'Water Drop',
-        'Water Wake',
-        'Witch Magic',
-        'Wolf Hearts',
-        'Wolf Shake',
-        'Wolf Smoke',
-      ],
-      'Angry Villager',
-    ).setTooltip(
+    new ListValue('Particle', 'particle', getParticles, getParticles()[0]).setTooltip(
       'The type of particle to display. Particle effects that show the DX, DY, and DZ options are not compatible with Cauldron',
     ),
   );
 
   component.data.push(
-    new ListValue('Material', 'material', getMaterials, 'Dirt')
+    new ListValue('Material', 'material', getMaterials, getMaterials()[0])
       .requireValue('particle', ['Block Crack', 'Icon Crack'])
       .setTooltip('The material to use for the Block Crack or Icon Crack particles'),
   );
@@ -252,51 +194,61 @@ const addParticleOptions = (component) => {
   );
 
   // Reflection particle data
+  // Note: Some of the particles are not in / not matching the original SkillAPI's REFLECT_PARTICLES map in ParticleHelper.java
+  // However, they are still usable because all the map does is to convert their names to the enum names (in the latest version)
+  // I thought "reflection" here means the particle is sent by a packet using Reflection, while the other particles
+  // are sent by Spigot API. However, SkillAPI actually uses Reflection to send packets for ALL particles types.
+  // Maybe Eniripsa didn't cleanup the code? I don't know. I don't want to bother digging its history.
+  // And in the editor, only the reflection particles can have dx, dy, dz, speed, amount and visible-radius options.
+  // It's very confusing. I haven't verfied and tested them, but I know visible-radius and amount must be valid for all particles.
+  // However, speed, dx, dy, dz are only useful for certain types. I'm assuming the types in the
+  // list below are the ones that support them (speed, dx, dy, dz).
+  // FIXME: Verify and test the dx, dy, dz options for all particles.
   const reflectList = [
-    'Angry Villager',
-    'Bubble',
+    'Villager Angry',
+    'Water Bubble',
     'Cloud',
     'Crit',
     'Damage Indicator',
-    'Death Suspend',
+    'Suspended Depth',
     'Dragon Breath',
     'Drip Lava',
     'Drip Water',
     'Enchantment Table',
     'End Rod',
-    'Explode',
+    'Explosion Normal',
     'Fireworks Spark',
     'Flame',
     'Footstep',
-    'Happy Villager',
-    'Hear',
-    'Huge Explosion',
-    'Instant Spell',
-    'Large Explode',
-    'Large Smoke',
+    'Villager Happy',
+    'Heart',
+    'Explosion Huge',
+    'Spell Instant',
+    'Explosion Large',
+    'Smoke Large',
     'Lava',
-    'Magic Crit',
-    'Mob Spell',
-    'Mob Spell Ambient',
+    'Crit Magic',
+    'Spell Mob',
+    'Spell Mob Ambient',
     'Note',
     'Portal',
-    'Red Dust',
+    'Redstone',
     'Slime',
-    'Snowball Poof',
+    'Snowball',
     'Snow Shovel',
     'Spell',
-    'Splash',
-    'Suspend',
+    'Water Splash',
+    'Suspended',
     'Sweep Attack',
     'Town Aura',
     'Water Drop',
     'Water Wake',
-    'Witch Magic',
+    'Spell Witch',
   ];
   component.data.push(
-    new IntValue('Visible Radius', 'visible-radius', 25)
-      .requireValue('particle', reflectList)
-      .setTooltip('How far away players can see the particles from in blocks'),
+    new IntValue('Visible Radius', 'visible-radius', 25).setTooltip(
+      'How far away players can see the particles from in blocks',
+    ),
   );
   component.data.push(
     new DoubleValue('DX', 'dx', 0)
@@ -327,11 +279,9 @@ const addParticleOptions = (component) => {
       ),
   );
   component.data.push(
-    new DoubleValue('Packet Amount', 'amount', 1)
-      .requireValue('particle', reflectList)
-      .setTooltip(
-        'A packet variable that varies between particles. Setting this to 0 lets you control the color of some particles.',
-      ),
+    new DoubleValue('Packet Amount', 'amount', 1).setTooltip(
+      'A packet variable that varies between particles. Setting this to 0 lets you control the color of some particles.',
+    ),
   );
 };
 
@@ -424,65 +374,22 @@ const addEffectOptions = (component, optional) => {
       new ListValue(
         'Particle Type',
         '-particle-type',
-        [
-          'BARRIER',
-          'BLOCK_CRACK',
-          'CLOUD',
-          'CRIT',
-          'CRIT_MAGIC',
-          'DAMAGE_INDICATOR',
-          'DRAGON_BREATH',
-          'DRIP_LAVA',
-          'DRIP_WATER',
-          'ENCHANTMENT_TABLE',
-          'END_ROD',
-          'EXPLOSION_HUGE',
-          'EXPLOSION_LARGE',
-          'EXPLOSION_NORMAL',
-          'FIREWORKS_SPARK',
-          'FLAME',
-          'FOOTSTEP',
-          'HEART',
-          'LAVA',
-          'MOB_APPEARANCE',
-          'NOTE',
-          'PORTAL',
-          'REDSTONE',
-          'SLIME',
-          'SMOKE_NORMAL',
-          'SMOKE_LARGE',
-          'SNOWBALL',
-          'SNOW_SHOVEL',
-          'SPELL',
-          'SPELL_INSTANT',
-          'SPELL_MOB',
-          'SPELL_MOB_AMBIENT',
-          'SPELL_WITCH',
-          'SUSPEND_DEPTH',
-          'SUSPENDED',
-          'SWEEP_ATTACK',
-          'TOWN_AURA',
-          'VILLAGER_ANGRY',
-          'VILLAGER_HAPPY',
-          'WATER_BUBBLE',
-          'WATER_SPLASH',
-          'WATER_WAKE',
-        ],
-        'BARRIER',
+        getParticles(),
+        getParticles()[0],
       ).setTooltip('The type of particle to use'),
     ),
   );
   component.data.push(
     opt(
-      new ListValue('Particle Material', '-particle-material', getMaterials, 'WOOD')
-        .requireValue('-particle-type', ['BLOCK_CRACK'])
+      new ListValue('Particle Material', '-particle-material', getMaterials, 'Wood')
+        .requireValue('-particle-type', ['Block Crack', 'Icon Crack'])
         .setTooltip('The material to use for the particle'),
     ),
   );
   component.data.push(
     opt(
       new IntValue('Particle Data', '-particle-data', 0)
-        .requireValue('-particle-type', ['BLOCK_CRACK'])
+        .requireValue('-particle-type', ['Block Crack', 'Icon Crack'])
         .setTooltip('The data value for the material used by the particle'),
     ),
   );
